6d42c197f090315eb6832d96d1c920f8f22fda4f
SuperSodaSea <bobby825@126.com>
2018-01-24 12:07:48 +0800

    Update .travis.yml

--- Main.cpp
+++ Main.cpp
@@ -3,7 +3,11 @@
 int main() {
     
     std::cout << "Hello, CI!" << std::endl;
-    std::cout << "C++: " << __cplusplus << std::endl;
+    
+    // C++14
+    auto func = [count = 0]() mutable { std::cout << ++count << std::endl; };
+    func(), func(), func();
+    
     return 0;
     
 }
