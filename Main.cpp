b6cbcb8d122502094c2881dd78ef6c5ff0f9ba8d
SuperSodaSea <bobby825@126.com>
2018-01-24 12:44:15 +0800

    Update .travis.yml

--- Main.cpp
+++ Main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <type_traits>
 
 int main() {
     
@@ -7,6 +8,7 @@ int main() {
     // C++14
     auto func = [count = 0]() mutable { std::cout << ++count << std::endl; };
     func(), func(), func();
+    std::cout << std::make_unsigned_t<int>(-1) << std::endl;
     
     return 0;
     
